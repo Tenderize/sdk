de9950e8f11c37ca0812f7d0aae7b669bc3ce890
vik <kashyapvikas20@gmail.com>
2024-03-08 16:54:13 +0100

    feat: Add @radix-ui/react-icons as a dependency and update TokenSelector component

--- packages/sdk/lib/components/TokenSelector/index.tsx
+++ packages/sdk/lib/components/TokenSelector/index.tsx
@@ -1,37 +1,62 @@
-import { TOKENS } from "@lib/constants";
+import { TOKENS, TokenSlugEnums } from "@lib/constants";
 import { useTokenStore } from "@lib/hooks";
 import { type Token } from "@lib/types";
+import { CheckIcon, ChevronDownIcon } from "@radix-ui/react-icons";
 import {
+  Box,
   Button,
   DropdownMenu as DropdownMenuRadix,
+  Flex,
   Text,
 } from "@radix-ui/themes";
-import React, { type FC, useState } from "react";
+import React, { useEffect, useState, type FC } from "react";
 
 type DropdownMenuRadixProps = React.ComponentProps<
   typeof DropdownMenuRadix.Root &
-  typeof DropdownMenuRadix.Content &
-  typeof DropdownMenuRadix.Item &
-  typeof DropdownMenuRadix.Trigger
+    typeof DropdownMenuRadix.Content &
+    typeof DropdownMenuRadix.Item &
+    typeof DropdownMenuRadix.Trigger
 >;
-
-interface SelectedItem {
+type SelectedItem = {
   Icon: FC;
   name: string;
+};
+interface TokenSelectorProps extends DropdownMenuRadixProps {
+  defaultValue: TokenSlugEnums;
 }
 
-export const TokenSelector: FC<DropdownMenuRadixProps> = () => {
+export const TokenSelector: FC<TokenSelectorProps> = (props) => {
+  const { defaultValue } = props;
+
+  const findDefaultItem = Object.values(TOKENS).find(
+    (token) => token.slug === defaultValue
+  );
+
+  useEffect(() => {
+    if (findDefaultItem) {
+      setSelectedItem({
+        Icon: () => (
+          <img
+            width={25}
+            height={25}
+            src={findDefaultItem.img.token}
+            alt={findDefaultItem.name}
+          />
+        ),
+        name: findDefaultItem.currency,
+      });
+    }
+  }, [findDefaultItem]);
+
   const [selectedItem, setSelectedItem] = useState<SelectedItem | undefined>();
   const { setSelectedToken } = useTokenStore();
 
   const tokensData = Object.values(TOKENS).map((token: Token) => {
     return {
       Icon: () => (
-        <Text style={{ color: token.color }}>
-          <img src={token.img.token} alt={token.name} />
-        </Text>
+        <img width={25} height={25} src={token.img.token} alt={token.name} />
       ),
-      name: token.name,
+      name: token.currency,
       slug: token.slug,
     };
   });
@@ -41,24 +66,39 @@ export const TokenSelector: FC<DropdownMenuRadixProps> = () => {
   return (
     <DropdownMenuRadix.Root>
       <DropdownMenuRadix.Trigger>
-        <Button variant="outline">
-          {Icon && <Icon />}
-          <Text>{name || "Select Token"}</Text>
+        <Button variant="surface" size={"3"}>
+          <Flex gap="2" align="center" justify={"between"}>
+            {!!Icon && <Icon />}
+            <Text>{name || "Select Token"}</Text>
+            <ChevronDownIcon />
+          </Flex>
         </Button>
       </DropdownMenuRadix.Trigger>
       <DropdownMenuRadix.Content>
-        {tokensData.map((item, index) => (
-          <DropdownMenuRadix.Item
-            onSelect={() => {
-              setSelectedItem(item);
-              setSelectedToken(item.slug);
-            }}
-            key={index}
-          >
-            {item.Icon && <item.Icon />}
-            <Text>{item.name}</Text>
-          </DropdownMenuRadix.Item>
-        ))}
+        <Flex direction="column" gap="4">
+          {tokensData.map((item, index) => (
+            <DropdownMenuRadix.Item
+              style={{
+                ...(selectedItem?.name === item.name && { opacity: 0.5 }),
+              }}
+              onSelect={() => {
+                setSelectedItem(item);
+                setSelectedToken(item.slug);
+              }}
+              key={index}
+            >
+              <Box position={"relative"}>
+                <Flex gap="4" align="center" justify={"between"}>
+                  {item.Icon && <item.Icon />}
+                  <Flex gap="1" align={"center"}>
+                    <Text>{item.name}</Text>
+                    {selectedItem?.name === item.name && <CheckIcon />}
+                  </Flex>
+                </Flex>
+              </Box>
+            </DropdownMenuRadix.Item>
+          ))}
+        </Flex>
       </DropdownMenuRadix.Content>
     </DropdownMenuRadix.Root>
   );
