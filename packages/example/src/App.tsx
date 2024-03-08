45db9e90b6bc3163c4c7f37c57493c5d96a56be1
kyriediculous <vergauwennico@gmail.com>
2024-03-08 01:12:36 +0100

    feat: fix example app with SDK changes

--- packages/example/src/App.tsx
+++ packages/example/src/App.tsx
@@ -1,25 +1,6 @@
 import { Flex } from "@radix-ui/themes";
-import { Tabs } from "@tenderize/sdk";
 import { Card } from "@tenderize/sdk";
-import { Stake, Swap, Unstake } from "@tenderize/sdk";
-
-const TABS_DATA = [
-  {
-    name: "Stake",
-    content: () => <Stake />,
-    value: "stake",
-  },
-  {
-    name: "Unstake",
-    content: () => <Unstake />,
-    value: "unstake",
-  },
-  {
-    name: "Swap",
-    content: () => <Swap />,
-    value: "swap",
-  },
-];
+import { Tenderize } from "@tenderize/sdk";
 
 function App() {
   return (
@@ -30,7 +11,7 @@ function App() {
       height={"auto"}
     >
       <Card size={"1"} className="w-[40%]">
-        <Tabs tabsData={TABS_DATA} />
+        <Tenderize />
       </Card>
     </Flex>
   );
--- packages/example/src/main.tsx
+++ packages/example/src/main.tsx
@@ -2,15 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
-import { Web3Provider } from '@tenderize/sdk'
+import { Web3Provider, createTenderizeConfig } from '@tenderize/sdk'
+
 import {
   ThemeProvider
 } from '@tenderize/sdk'
 
+const config = createTenderizeConfig({
+  apiKey: import.meta.env.VITE_ALCHEMY_API_KEY as string
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
-      <Web3Provider>
+      <Web3Provider config={config}>
         <App />
       </ Web3Provider>
     </ ThemeProvider >
