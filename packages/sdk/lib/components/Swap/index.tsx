d859719d3c66fe736345ed4f450004e4e9a04ff0
vik <kashyapvikas20@gmail.com>
2024-03-11 20:13:16 +0100

    Fix tokenSlug import and add tokenSlug prop to MaxBalanceButton component

--- packages/sdk/lib/components/Swap/index.tsx
+++ packages/sdk/lib/components/Swap/index.tsx
@@ -1,23 +1,29 @@
-import { Flex } from "@radix-ui/themes";
-import MaxBalanceButton from "@lib/components/MaxBalanceButton";
 import { Button } from "@lib/components/Button";
+import MaxBalanceButton from "@lib/components/MaxBalanceButton";
 import { OutputField } from "@lib/components/OutputField";
-import { type FC, useState } from "react";
-import { useSelectedToken, useQuote } from "@lib/hooks";
+import { useQuote, useSelectedToken } from "@lib/hooks";
+import { Flex } from "@radix-ui/themes";
+import { useState, type FC } from "react";
 import { formatEther } from "viem";
 
 export const Swap: FC = () => {
   const [amount, setAmount] = useState<bigint>(0n);
   const { token, tenderizer } = useSelectedToken();
-  const { quote, isLoading, isError } = useQuote(token.slug, tenderizer, amount, token.chainId);
-  isLoading; isError;
+  const { quote, isLoading, isError } = useQuote(
+    token.slug,
+    tenderizer,
+    amount,
+    token.chainId
+  );
+  isLoading;
+  isError;
   return (
     <Flex direction="column" gap="2">
       <MaxBalanceButton
         tokenAddress={tenderizer}
+        tokenSlug={token.slug}
         handleInputChange={(value: bigint) => {
-          if (value && value != amount)
-            setAmount(value)
+          if (value && value != amount) setAmount(value);
         }}
       />
       <OutputField value={formatEther(quote.out ?? 0n)} disabled />
