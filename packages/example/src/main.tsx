e849007320af7eeae24ed1e87ede3b606ffa8834
kyriediculous <vergauwennico@gmail.com>
2024-03-12 14:52:46 +0100

    feat: include new config options in example app

--- packages/example/src/main.tsx
+++ packages/example/src/main.tsx
@@ -2,20 +2,46 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
-import { Web3Provider, createTenderizeConfig } from "@tenderize/sdk";
+import { TenderizeProvider, TokenSlugEnums, createTenderizeConfig, type TenderizersConfig, type TenderizeChains } from "@tenderize/sdk";
 
 import { ThemeProvider } from "@tenderize/sdk";
+import { mainnet, arbitrum } from "wagmi/chains";
+import { http } from "wagmi";
+
+
+const TENDERIZERS: TenderizersConfig = {
+  [TokenSlugEnums.MATIC]: "0x43ef285f5e27d8ca978a7e577f4ddf52147eb77b",
+  [TokenSlugEnums.LIVEPEER]: "0x43ef285f5e27d8ca978a7e577f4ddf52147eb77b",
+  [TokenSlugEnums.GRAPH]: "0x43ef285f5e27d8ca978a7e577f4ddf52147eb77b",
+};
+
+
+
+const mainnetChain = { ...mainnet, id: process.env.LOCALHOST ? 13371 : mainnet.id }
+const arbitrumChain = { ...arbitrum, id: process.env.LOCALHOST ? 133742161 : arbitrum.id }
+
+const CHAINS: TenderizeChains = {
+  [TokenSlugEnums.MATIC]: mainnetChain,
+  [TokenSlugEnums.LIVEPEER]: arbitrumChain,
+  [TokenSlugEnums.GRAPH]: arbitrumChain,
+};
 
 const config = createTenderizeConfig({
-  apiKey: import.meta.env.VITE_ALCHEMY_API_KEY as string,
+  tenderizers: TENDERIZERS,
+  chains: CHAINS,
+  transports: {
+    [mainnetChain.id]: http("http://localhost:8546"),
+    [arbitrumChain.id]: http("http://localhost:8545"),
+  }
+  // apiKey: import.meta.env.VITE_ALCHEMY_API_KEY as string,
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
-      <Web3Provider config={config}>
+      <TenderizeProvider config={config}>
         <App />
-      </Web3Provider>
+      </TenderizeProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
