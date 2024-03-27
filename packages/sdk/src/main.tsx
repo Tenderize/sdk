f947999cd8384eeeef7183a4d9bd3575a1916ccf
kyriediculous <vergauwennico@gmail.com>
2024-03-27 19:18:09 +0100

    feat: build command for example app

--- packages/sdk/src/main.tsx
+++ packages/sdk/src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { http } from "wagmi"
 import { TokenSlugEnums } from "@lib/constants";
 import { TenderizeProvider, Web3Provider } from "@lib/contexts";
 
