4dccb9ad33a96336848ec428ad6091b701e0be09
vik <kashyapvikas20@gmail.com>
2024-05-02 12:11:53 +0200

    feat: tender swap functionality added

--- packages/sdk/lib/components/Swap/index.tsx
+++ packages/sdk/lib/components/Swap/index.tsx
@@ -4,16 +4,28 @@ import {
   InputField,
   MaxBalanceButton,
   OutputField,
+  SwitchChainButton,
   TokenSelector,
 } from "@lib/components";
 import { useChainId, useTenderizer } from "@lib/config/store";
 import { ActionEnums } from "@lib/constants";
 import { useSelectedToken } from "@lib/contexts";
-import { useERC20Balance, useQuote } from "@lib/hooks";
+import {
+  useERC20Approve,
+  useERC20Balance,
+  useQuote,
+  useSwap,
+} from "@lib/hooks";
+import { formatAmount, formatFloatstring } from "@lib/utils/floats";
+import { isMutationPending } from "@lib/utils/global";
+import {
+  CheckCircledIcon,
+  ExclamationTriangleIcon,
+} from "@radix-ui/react-icons";
 import { Flex, Text } from "@radix-ui/themes";
-import { useState, type FC } from "react";
-import { formatEther, parseEther } from "viem";
-import { useAccount } from "wagmi";
+import { useEffect, useState, type FC } from "react";
+import { formatEther, parseEther, type Address } from "viem";
+import { useAccount, useChainId as useCurrentChainId } from "wagmi";
 
 export const Swap: FC = () => {
   const [amount, setAmount] = useState<string>("");
@@ -21,13 +33,52 @@ export const Swap: FC = () => {
   const tenderizer = useTenderizer(token.slug);
   const chainId = useChainId(token.slug);
   const { address: userAddress } = useAccount();
+  const currentChainId = useCurrentChainId();
+
   const { balance } = useERC20Balance(tenderizer, userAddress, chainId);
+
+  const {
+    mutate: approve,
+    data: approval,
+    status: approveStatus,
+  } = useERC20Approve(
+    tenderizer,
+    token.tenderswap as Address,
+    parseEther(amount),
+    chainId
+  );
+
   const { quote } = useQuote(
     token.slug,
     tenderizer,
     parseEther(amount),
     chainId
   );
+
+  const { mutate: swap, status: swapStatus } = useSwap(
+    token?.tenderswap as Address,
+    tenderizer,
+    parseEther(amount),
+    quote.out,
+    chainId
+  );
+  // used to rest the amount after a successful swap
+  useEffect(() => {
+    if (swapStatus === "success") {
+      setAmount("");
+    }
+  }, [swapStatus]);
+
+  const swapFeePercentage = quote?.fee
+    ? formatFloatstring(
+        (
+          (parseFloat(formatEther(quote.fee)) /
+            parseFloat(formatEther(quote.out))) *
+          100
+        ).toString(),
+        4
+      )
+    : 0;
 
   return (
     <Flex gap="2" content="between" direction="column">
@@ -69,14 +120,91 @@ export const Swap: FC = () => {
                 </Flex>
               }
             />
+
+            <span className="text-sm">{`Expected swap fees: ${formatAmount(
+              quote.fee
+            )} t${token?.currency} (${swapFeePercentage} %)`}</span>
           </Flex>
         }
         callOutActionChildren={
-          <Flex gap="2" width="100%">
-            <Button style={{ width: "100%" }} size="4" variant="solid">
-              Swap {token.currency}
-            </Button>
-          </Flex>
+          <div className="flex flex-col gap-2 w-full">
+            <Flex gap="2" width="100%">
+              {(() => {
+                if (currentChainId !== chainId) {
+                  return <SwitchChainButton requiredChainId={chainId} />;
+                }
+
+                if (swapStatus === "success") {
+                  return (
+                    <Button
+                      style={{ width: "100%", pointerEvents: "none" }}
+                      size="4"
+                      variant="soft"
+                      color="green"
+                    >
+                      <Flex gap="2" align="center">
+                        <CheckCircledIcon />
+                        <Text>Staked {token.currency}</Text>
+                      </Flex>
+                    </Button>
+                  );
+                }
+                if (!approval) {
+                  return (
+                    <Button
+                      className={
+                        isMutationPending(approveStatus) ? "animate-pulse" : ""
+                      }
+                      disabled={!amount || isMutationPending(approveStatus)}
+                      style={{ width: "100%" }}
+                      size="4"
+                      onClick={() => approve()}
+                      variant="solid"
+                    >
+                      {isMutationPending(approveStatus) ? (
+                        <>Approving {token.currency}...</>
+                      ) : (
+                        <>Approve {token.currency}</>
+                      )}
+                    </Button>
+                  );
+                }
+
+                return (
+                  <Button
+                    className={
+                      isMutationPending(swapStatus) ? "animate-pulse" : ""
+                    }
+                    disabled={!amount || isMutationPending(swapStatus)}
+                    style={{ width: "100%" }}
+                    size="4"
+                    onClick={() => swap?.()}
+                    variant="solid"
+                  >
+                    {isMutationPending(swapStatus) ? (
+                      <>Swaping {token.currency}...</>
+                    ) : (
+                      <>Swap {token.currency}</>
+                    )}
+                  </Button>
+                );
+              })()}
+            </Flex>
+            <div
+              className={`${
+                parseFloat(swapFeePercentage.toString()) > 0.02 ? "hidden" : ""
+              } bg-orange-100 p-4 my-2 dark:bg-transparent rounded-md dark:text-light-white flex items-center gap-4 ${
+                quote && quote.fee > 0.05 ? "" : "hidden"
+              }`}
+            >
+              <ExclamationTriangleIcon className="text-orange-700 text-lg" />
+              <div>
+                <span className="font-bold text-sm text-orange-700">
+                  Swap Fee {swapFeePercentage} %
+                </span>
+              </div>
+            </div>
+          </div>
         }
       ></CalloutLayout>
     </Flex>
--- packages/sdk/lib/hooks/swap.ts
+++ packages/sdk/lib/hooks/swap.ts
@@ -1,37 +1,164 @@
-import { type Address } from 'viem';
-import { TenderSwapAbi } from '@lib/abis';
-import { TOKENS } from '@lib/constants/tokens';
-import { useReadContract } from 'wagmi';
-import { TokenSlugEnums } from '@lib/constants/enums';
+import { TenderSwapAbi, TenderizerAbi } from "@lib/abis";
+import { TokenSlugEnums } from "@lib/constants/enums";
+import { TOKENS } from "@lib/constants/tokens";
+import { type ERC2612Permit } from "@lib/utils";
+import { useMutation } from "@tanstack/react-query";
+import {
+  getPublicClient,
+  getWalletClient,
+  waitForTransactionReceipt,
+  writeContract,
+} from "@wagmi/core";
+import { Interface } from "ethers/lib/utils";
+import { type Address, type Hex } from "viem";
+import { simulateContract } from "viem/actions";
+import { useConfig, useReadContract, type Config } from "wagmi";
 
 export type UseQuote = (
-    asset: TokenSlugEnums,
-    tenderizer: Address,
-    amount: bigint,
-    chainId: number
+  asset: TokenSlugEnums,
+  tenderizer: Address,
+  amount: bigint,
+  chainId: number
 ) => {
-    quote: { out: bigint; fee: bigint };
-    isLoading: boolean;
-    isError: boolean;
+  quote: { out: bigint; fee: bigint };
+  isLoading: boolean;
+  isError: boolean;
 };
 
 export const useQuote: UseQuote = (asset, tenderizer, amount, chainId) => {
-    const { data, isLoading, isError } = useReadContract(
-        {
-            address: TOKENS[asset]?.tenderswap,
-            functionName: 'quote',
-            args: [tenderizer, amount],
-            abi: TenderSwapAbi,
-            chainId,
-            query: {
-                enabled: !!amount
-            }
-        }
-    );
+  const { data, isLoading, isError } = useReadContract({
+    address: TOKENS[asset]?.tenderswap,
+    functionName: "quote",
+    args: [tenderizer, amount],
+    abi: TenderSwapAbi,
+    chainId,
+    query: {
+      enabled: !!amount,
+    },
+  });
 
-    return {
-        quote: { out: data?.[0] ?? 0n, fee: data?.[1] ?? 0n },
-        isLoading,
-        isError,
-    };
+  return {
+    quote: { out: data?.[0] ?? 0n, fee: data?.[1] ?? 0n },
+    isLoading,
+    isError,
+  };
+};
+
+export const useSwap = (
+  tenderswap: Address,
+  tenderizer: Address,
+  amount: bigint,
+  minOut: bigint,
+  chainId: number,
+  permit?: ERC2612Permit
+) => {
+  const wagmiConfig = useConfig();
+  const mutation = useMutation({
+    mutationFn: async () => {
+      return permit
+        ? swapWithPermit(
+            tenderswap,
+            tenderizer,
+            amount,
+            minOut,
+            permit,
+            chainId,
+            wagmiConfig
+          )
+        : swapWithApprove(
+            tenderswap,
+            tenderizer,
+            amount,
+            minOut,
+            chainId,
+            wagmiConfig
+          );
+    },
+    onSuccess: () => {
+      // Reset the mutation status to idle after success
+      setTimeout(() => {
+        mutation.reset();
+      }, 2000);
+    },
+  });
+
+  return mutation;
+};
+
+const swapWithPermit = async (
+  tenderswap: Address,
+  tenderizer: Address,
+  amount: bigint,
+  minOut: bigint,
+  permit: ERC2612Permit,
+  chainId: number,
+  wagmiConfig: Config
+) => {
+  const ITenderizer = new Interface(TenderizerAbi);
+  const publicClient = getPublicClient(wagmiConfig, { chainId });
+
+  if (!publicClient) return;
+
+  try {
+    const { request } = await simulateContract(publicClient, {
+      address: tenderswap,
+      abi: TenderSwapAbi,
+      functionName: "multicall",
+      args: [
+        [
+          ITenderizer.encodeFunctionData("selfPermit", [
+            tenderizer,
+            permit.value,
+            permit.deadline,
+            permit.v,
+            permit.r,
+            permit.s,
+          ]) as Hex,
+          ITenderizer.encodeFunctionData("swap", [
+            tenderizer,
+            amount,
+            minOut,
+          ]) as Hex,
+        ],
+      ],
+    });
+
+    const hash = await writeContract(wagmiConfig, request);
+
+    return hash;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+const swapWithApprove = async (
+  tenderswap: Address,
+  tenderizer: Address,
+  amount: bigint,
+  minOut: bigint,
+  chainId: number,
+  wagmiConfig: Config
+) => {
+  const publicClient = getPublicClient(wagmiConfig, { chainId });
+  const signer = await getWalletClient(wagmiConfig);
+  if (!publicClient) return;
+
+  try {
+    const { request: swap } = await simulateContract(signer, {
+      address: tenderswap,
+      abi: TenderSwapAbi,
+      functionName: "swap",
+      args: [tenderizer, amount, minOut],
+    });
+
+    const hash = await writeContract(wagmiConfig, swap);
+
+    await waitForTransactionReceipt(wagmiConfig, { hash, chainId });
+
+    return hash;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
