321c028b93eeb3c8748e4c33c069e74a5bf62391
vik <kashyapvikas20@gmail.com>
2024-05-10 13:43:53 +0200

    ref: tabs UI refactored, GRT dropped  from sdk config

--- packages/sdk/lib/components/Tabs/index.tsx
+++ packages/sdk/lib/components/Tabs/index.tsx
@@ -1,4 +1,4 @@
-import { Tabs as TabsRadix } from "@radix-ui/themes";
+import * as TabsRadix from "@radix-ui/react-tabs";
 import type { ComponentProps, FC } from "react";
 import { BalanceCard } from "..";
 
@@ -16,10 +16,13 @@ export const Tabs: FC<Props> = (props) => {
 
   return (
     <TabsRadix.Root {...rest} defaultValue={tabsData[0]?.value}>
-      <TabsRadix.List className="justify-around bg-background">
+      <TabsRadix.List className="justify-center bg-card rounded-lg items-center flex rounded-lg gap-0 py-2">
         {tabsData.map((tab) => (
           <TabsRadix.Trigger
-            className="text-xl text-primary-foreground data-[state=active]:before:text-primary data-[state=active]:before:bg-primary "
+            className="inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-5 text-xl
+            text-primary-foreground data-[state=active]:text-primary  data-[state=active]:bg-background w-[32.5%] h-[20px]
+          rounded-lg  cursor-pointer data-[state=active]:shadow-md hover:bg-transparent transition-all
+          data-[state=active]:border border-transparent border-b-primary "
             key={tab.value}
             value={tab.value}
           >
