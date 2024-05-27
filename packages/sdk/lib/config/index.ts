8f6972171f199a58bcceab07a0e7b79c7017344b
vik <kashyapvikas20@gmail.com>
2024-05-27 11:23:13 +0200

    feat: Add tokens to Tenderize config

--- packages/sdk/lib/config/index.ts
+++ packages/sdk/lib/config/index.ts
@@ -1,59 +1,71 @@
-import type { TenderizeChains, TenderizeConfig, TenderizeConfigOptions } from '@lib/types';
-import { http, createConfig } from 'wagmi'
-import { mainnet, arbitrum, type Chain } from "wagmi/chains";
+import type {
+  TenderizeChains,
+  TenderizeConfig,
+  TenderizeConfigOptions,
+} from "@lib/types";
 import { getDefaultConfig } from "connectkit";
+import { createConfig, http } from "wagmi";
+import { arbitrum, mainnet, type Chain } from "wagmi/chains";
 
 interface CreateTenderizeConfig {
-    (options: TenderizeConfigOptions): TenderizeConfig;
+  (options: TenderizeConfigOptions): TenderizeConfig;
 }
 
 export const createTenderizeConfig: CreateTenderizeConfig = (config) => {
-    if (!Object.keys(config.tenderizers === Object.keys(config.chains))) {
-        throw new Error("Tenderizers and chains configuration must match supported tokens")
+  if (!Object.keys(config.tenderizers === Object.keys(config.chains))) {
+    throw new Error(
+      "Tenderizers and chains configuration must match supported tokens"
+    );
+  }
+
+  function toChainArray(chains: TenderizeChains): [Chain, ...Chain[]] {
+    const chainsArray = Object.values(chains);
+    if (chainsArray.length > 0) {
+      // This assertion is safe because we've checked that chains.length > 0
+      return [...new Set(chainsArray)] as [Chain, ...Chain[]];
+    } else {
+      // Handle the case where the array is empty. Returning 'undefined' is one way to handle it.
+      return [mainnet];
     }
+  }
 
-    function toChainArray(chains: TenderizeChains): [Chain, ...Chain[]] {
-        const chainsArray = Object.values(chains);
-        if (chainsArray.length > 0) {
-            // This assertion is safe because we've checked that chains.length > 0
-            return [...new Set(chainsArray)] as [Chain, ...Chain[]];
-        } else {
-            // Handle the case where the array is empty. Returning 'undefined' is one way to handle it.
-            return [mainnet];
-        }
-    }
+  const defaultChains = [mainnet, arbitrum];
+  const wagmiChains: [Chain, ...Chain[]] = toChainArray(config.chains);
 
-    const defaultChains = [mainnet, arbitrum]
-    const wagmiChains: [Chain, ...Chain[]] = toChainArray(config.chains)
+  return {
+    tenderizers: config.tenderizers,
+    chains: config.chains,
+    web3: createConfig(
+      getDefaultConfig({
+        // Your dApps chains
+        chains: (config.apiKey ? defaultChains : wagmiChains) as [
+          Chain,
+          ...Chain[]
+        ],
+        transports: config.transports || {
+          // RPC URL for each chain
+          [mainnet.id]: http(
+            `https://eth-mainnet.g.alchemy.com/v2/${config.apiKey}`
+          ),
+          [arbitrum.id]: http(
+            `https://arb-mainnet.g.alchemy.com/v2/${config.apiKey}`
+          ),
+        },
 
-    return {
-        tenderizers: config.tenderizers,
-        chains: config.chains,
-        web3: createConfig(
-            getDefaultConfig({
-                // Your dApps chains
-                chains: (config.apiKey ? defaultChains : wagmiChains) as [Chain, ...Chain[]],
-                transports: config.transports || {
-                    // RPC URL for each chain
-                    [mainnet.id]: http(
-                        `https://eth-mainnet.g.alchemy.com/v2/${config.apiKey}`,
-                    ),
-                    [arbitrum.id]: http(
-                        `https://arb-mainnet.g.alchemy.com/v2/${config.apiKey}`,
-                    ),
-                },
+        // Required API Keys
+        walletConnectProjectId: import.meta.env
+          .VITE_WALLETCONNECT_PROJECT_ID as string,
 
-                // Required API Keys
-                walletConnectProjectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID as string,
+        // Required App Info
+        appName: config.appName || "Tenderize SDK",
 
-                // Required App Info
-                appName: config.appName || "Tenderize SDK",
-
-                // Optional App Info
-                appDescription: config.appDescription || "Built using the Tenderize SDK",
-                appUrl: config.appUrl || "https://tenderize.me", // your app's url
-                appIcon: config.appIcon || "https://app.tenderize.me/wagyu.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
-            }),
-        )
-    };
-}
+        // Optional App Info
+        appDescription:
+          config.appDescription || "Built using the Tenderize SDK",
+        appUrl: config.appUrl || "https://tenderize.me", // your app's url
+        appIcon: config.appIcon || "https://app.tenderize.me/wagyu.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+      })
+    ),
+    ...(config?.tokens && { tokens: config.tokens }),
+  };
+};
--- packages/sdk/lib/types/config.ts
+++ packages/sdk/lib/types/config.ts
@@ -5,37 +5,40 @@ import type { Config, CreateConfigParameters } from "wagmi";
 // Assuming CreateConfigParameters already includes optional "chains" and "transports",
 // we make a version that requires these properties to be non-nullable.
 type ConfigWithChainsAndTransports = CreateConfigParameters & {
-    chains: NonNullable<CreateConfigParameters['chains']>;
-    transports: NonNullable<CreateConfigParameters['transports']>;
+  chains: NonNullable<CreateConfigParameters["chains"]>;
+  transports: NonNullable<CreateConfigParameters["transports"]>;
 };
 
 // Configuration when apiKey is provided, making "chains" and "transports" explicitly optional
-type ConfigWithApiKey = Omit<CreateConfigParameters, 'chains' | 'transports'> & {
-    apiKey: string;
-    chains?: never;
-    transports?: never;
+type ConfigWithApiKey = Omit<
+  CreateConfigParameters,
+  "chains" | "transports"
+> & {
+  apiKey: string;
+  chains?: never;
+  transports?: never;
 };
 
 export type TenderizeConfig = {
-    tenderizers: TenderizersConfig;
-    chains: TenderizeChains;
-    web3: Config
-}
+  tenderizers: TenderizersConfig;
+  chains: TenderizeChains;
+  web3: Config;
+  tokens?: TokenSlugEnums[];
+};
 
 export type TenderizersConfig = { [token in TokenSlugEnums]?: Address };
 export type TenderizeChains = { [token in TokenSlugEnums]?: Chain };
 
 // Define the conditional ConfigOptions type
 export type Web3ConfigOptions = {
-    appName?: string;
-    appDescription?: string;
-    appUrl?: string;
-    appIcon?: string;
+  appName?: string;
+  appDescription?: string;
+  appUrl?: string;
+  appIcon?: string;
 } & (ConfigWithApiKey | (ConfigWithChainsAndTransports & { apiKey?: never }));
 
-
-
 export type TenderizeConfigOptions = {
-    tenderizers: TenderizersConfig;
-    chains: TenderizeChains;
+  tenderizers: TenderizersConfig;
+  chains: TenderizeChains;
+  tokens?: TokenSlugEnums[];
 } & Omit<Web3ConfigOptions, "chains">;
