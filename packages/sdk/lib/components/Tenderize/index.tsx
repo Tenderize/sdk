1786b20de87ff23ffe789333be78109df0dfc080
vik <kashyapvikas20@gmail.com>
2024-05-28 15:17:44 +0200

    fix : routes issue while publishing sdk

--- packages/sdk/lib/components/Tenderize/index.tsx
+++ packages/sdk/lib/components/Tenderize/index.tsx
@@ -10,6 +10,13 @@ import { useTenderizeConfigStore } from "@lib/config/store";
 import type { TabEnum } from "@lib/hooks";
 import { type FC } from "react";
 
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
 const TABS_DATA = [
   {
     name: "Stake",
@@ -28,8 +35,9 @@ const TABS_DATA = [
   },
 ];
 
-export const Tenderize: FC = () => {
+const TenderizeRoute: FC = () => {
   const { activeTabs: activeTabsStore } = useTenderizeConfigStore();
+
   return (
     <div className="w-[95%] md:w-[650px] lg:w-[650px] pt-4 ">
       <div className="w-full mb-4">
@@ -49,4 +57,26 @@ export const Tenderize: FC = () => {
   );
 };
 
+export function Tenderize() {
+  const { activeTabs: activeTabsStore } = useTenderizeConfigStore();
+
+  const activeRoutes = activeTabsStore.map((tab) => (
+    <Route key={tab} path={tab} element={<TenderizeRoute />} />
+  ));
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<TenderizeRoute />}>
+        {activeRoutes}
+      </Route>
+    )
+  );
+  return (
+    <>
+      <div className="flex flex-col gap-4 items-center justify-center h-full pb-2">
+        <RouterProvider router={router} />
+      </div>
+    </>
+  );
+}
 export default Tenderize;
