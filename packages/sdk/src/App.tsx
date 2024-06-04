cfd2231b5f32ea0a0d087e53bec5a2c44ab4e9ba
vik <kashyapvikas20@gmail.com>
2024-06-04 10:44:02 +0200

    feat: remove stake capital branding from SDk, updated SDK pcakage version

--- packages/sdk/src/App.tsx
+++ packages/sdk/src/App.tsx
@@ -1,6 +1,4 @@
 import { Tenderize } from "@lib/components";
-import { FAQ } from "@lib/components/FAQ";
-import { Footer } from "@lib/components/Footer";
 import { Header } from "@lib/components/Header";
 
 function App() {
@@ -9,8 +7,6 @@ function App() {
       <div className="flex flex-col gap-4 items-center justify-center h-full pb-[250px]">
         <Header />
         <Tenderize />
-        <FAQ />
-        <Footer />
       </div>
     </>
   );
